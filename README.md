33be125d074a9181904369580a8ac346d1be2cf8
Itachi <itad0ri@proton.me>
2022-08-29 10:26:18 +0530

    1.1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dall-e-prompt-check
-Check if prompt is flagged by Open AI. Don't get Banned again!
+# 🪄 Dall E - Prompt Checker
+We all know Dall E is very strict when it comes to Prompts.
+Let's all be safe and use Open AI's moderation API to check if our prompt is flagged before we use it in Dall E.
+
+## ▶️ Demo
+![demo](https://i.imgur.com/hKQwNlF.gif)
+
+## ✅ Requirements
+- Python installed on your system. If it's not installed then visit: https://www.python.org/downloads/
+- Dall E/Open AI account to get API/Secret key.
+
+## 🔑 Getting API Key
+Visit: https://beta.openai.com/account/api-keys to get your API/Secret key.
+You can just click "copy" and then paste it in "api.txt" file.
+![get api key](https://i.imgur.com/UMzUdZ3.png)
+
+## 📃 License
+This project is under license from MIT. For more details, see the [License](https://choosealicense.com/licenses/mit/) file.
--- main.py
+++ main.py
@@ -1,16 +1,57 @@
 import openai
+from os import system, name, path
 
-openai.api_key = open("api.txt").read()
 
-prompt = input("Enter your prompt: ")
+key = open(path.join(path.dirname(__file__), "api.txt")).read()
+if key == "replace_with_your_api_key":
+    with open(path.join(path.dirname(__file__), "api.txt"), "w") as f:
+        f.write(input("Error: You forgot to enter your API key into 'api.txt' file.\n >> Please input your API key: "))
+    print("Info: API Key updated in 'api.txt' file for future use.\n")
+    key = open(path.join(path.dirname(__file__), "api.txt")).read()
 
-r = openai.Moderation.create(
-    input=prompt,
-)
+openai.api_key = key
 
-categories = r["results"][0]["categories"]
-if r["results"][0]["flagged"] is True:
-    print(" >> Flagged: ", end="")
-    [print(f"{x}, ", end="") for x in categories if categories[x] is True]
-else:
-    print(" >> Not Flagged.")
+
+class Colors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+
+def clear_console() -> None:
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+def flagged_or_not(my_prompt: str) -> None:
+    r = openai.Moderation.create(
+        input=my_prompt,
+    )
+
+    categories = r["results"][0]["categories"]
+    if r["results"][0]["flagged"] is True:
+        print(f" {Colors.RED}>> Flagged:{Colors.RESET} ", end="")
+        [print(f"{x}, ", end="") for x in categories if categories[x] is True]
+        print("\n")
+    else:
+        print(f" {Colors.GREEN}>> Not Flagged.{Colors.RESET}\n")
+
+
+if __name__ == '__main__':
+    while True:
+        prompt = input("Enter your prompt: ")
+        flagged_or_not(prompt)
+        again = input("Want to check more prompts? [y/n]: ")
+        if again.lower() == "n" or again.lower() == "no":
+            break
+        else:
+            clear_console()
